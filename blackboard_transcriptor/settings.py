70fa7234ec8fc5fd6bb267bb1a83b3ef7e705e08
Simon Bermudez <sabdz55@gmail.com>
2021-09-25 21:11:59 -0400

    added requirements.txt and DJANGO_SECRET_KEY

--- blackboard_transcriptor/settings.py
+++ blackboard_transcriptor/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j)-4*p=(+!0a*j@p=ll0s11m-g2!9(!&sac19em6_(6)u3q-vm'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', "notset")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
