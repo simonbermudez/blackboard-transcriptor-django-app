d13e4cdf973a61f6ae1cd1bd7d57ee8890288f0e
Simon Bermudez <sabdz55@gmail.com>
2021-09-26 08:40:20 -0400

    added searchbar and vercel

--- blackboard_transcriptor/settings.py
+++ blackboard_transcriptor/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', "notset")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', '.ngrok.io', 'localhost']
 
 
 # Application definition
