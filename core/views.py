7aeaa43953ea81e6b9cc2cc1387bb784c5cf1e34
Simon Bermudez <sabdz55@gmail.com>
2021-09-26 13:13:05 -0400

    updated video UI

--- core/views.py
+++ core/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
 
 # Create your views here.
 
 def Home(request):
-    return HttpResponse("<h1>Welcome to Blackboard Transcriptor</h1>")
+    return render(request, template_name='home.html', context = {})
